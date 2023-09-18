7683be579f1f79b9818ebbbbae41a1fbdff6d624
root <root@homeassistant.local.hass.io>
2023-09-18 14:15:44 -0500

    config files on 18-09-2023 14:15:44

--- ha_gitpush.sh
+++ ha_gitpush.sh
@@ -1,6 +1,8 @@
 # Go to /config folder or 
 # Change this to your Home Assistant config folder if it is different
+pwd
 cd /config
+pwd
 # Add all files to the repository with respect to .gitignore rules
 git add .
 # Commit changes with message with current date stamp
